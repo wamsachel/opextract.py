b4af42965e25838b001ea7733256621018fb882f
Lana <dietrich.wambach@protonmail.ch>
2016-11-18 01:40:40 -0700

    Small commit

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+// Simple helloworld
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!\n");
+	int a = 4;
+	int b = 3;
+	int c = (a * b) - (a+b);
+	printf("Out: %d\n", c);
+	return 0;
+}
